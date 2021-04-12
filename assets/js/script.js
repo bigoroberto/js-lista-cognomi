c5ce36b9abfebc9fc921b21522124cc644e1bcf3
bigoroberto <bigoroberto@gmail.com>
2021-04-12 16:31:18 +0200

    finito

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,21 +15,29 @@ var lista_cognomi = [
   "Messi"
 ];
 
-//con .sort faccio in modo che l'array mi esca in ordine alfabetico;
-
-
-
 //chiedo all'utente di inserire il suo cognome;
 var last_name_utente = prompt("Il tuo cognome?");
+
+//pusho-spedisco all'array la risposta dell'utente;
 lista_cognomi.push (last_name_utente);
+
+//con .sort faccio in modo che l'array mi esca in ordine alfabetico;
 lista_cognomi = lista_cognomi.sort();
+
+//ciclo con for, imposto la variabile i = 0 di default; imposto la condizione (i<lista_cognomi.length); vado ad incrementare per il numero totale di cogomi che ho nell'array (i++);
 for(var i = 0; i<lista_cognomi.length; i++){
+
+  //crao una variabile per identificare tutti i nomi della lista_cognomi, ogni oggetto oggetto della lista è "i" quindi scriverò: nuova var = lista-cognomi[i];
   var element_list = lista_cognomi[i];
+
+  //per non far partire da 0 aggiungo ad "i" + 1 e li racchiudo dentro una nuova variabile;
   var posizione_lista = i + 1;
+
+  //creo un'elemento precedente per stampare le "i" che ho gia all'interno dell'array; 
   var elemento_precedente = document.getElementById("lista").innerHTML;
 
-  document.getElementById("lista").innerHTML = elemento_precedente + "<li>" + element_list + " il tuo cognome si trova nella posizione n:  " + posizione_lista;
+  //faccio stampare gli elementi precedenti + il cognome dell'utente inserito ed il posizionamento. tutto racchiuso in un "<li></li>"
+  document.getElementById("lista").innerHTML = elemento_precedente + "<li>" + element_list + " il cognome si trova nella posizione n:  " + posizione_lista;
 
-
-
+  lista_cognomi[last_name_utente] = document.getElementById("lista").innerHTML = "il cognome dell'utente si trova nella posizione n:  "
 }
